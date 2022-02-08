ca8940502d634c4d2802156812ac40af7f9e1063
kzv <ivanivanockzv@gmail.com>
2022-02-09 00:19:41 +0500

    fix

--- source/server/main.js
+++ source/server/main.js
@@ -1,7 +1,13 @@
 "use strict";
 
 const customHandlers = {
-    custom: require("../server/p2p/custom")
+    custom: require("../server/p2p/custom"),
+    SSL_options: {
+        key: require("fs").readFileSync(__dirname+"/ssl_cert/privkey.pem"),
+        cert: require("fs").readFileSync(__dirname+"/ssl_cert/fullchain.pem")    
+    },
+    MAX_CONNECTIONS: 10,
+    my_portSSL: 10443
 }
 
 require("p2plib").StartServer(customHandlers);    
